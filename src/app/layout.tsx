dde7df2085304e195254785736ef33e63afcf4b7
Francis Benjamin <siscorule@gmail.com>
2024-11-21 02:42:29 +0100

    Updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Global WordPress Mastery',
-  description: 'Master WordPress development and launch your global career',
+  description: 'Master WordPress development and launch your global career.',
 }
 
 export default function RootLayout({
